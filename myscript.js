3cfa6421d8bc5f53d7cefefdebd56fa4ab48b0cd
Jeremy Johns <jez.johns@gmail.com>
2023-01-19 19:54:12 +0000

    resized images, added 3 js functions

--- myscript.js
+++ myscript.js
@@ -1,39 +1,48 @@
-// let myName = prompt("What is your name?");
-// console.log(myName);
-// let myExperience = prompt("How experienced a player are you? (1-4) 1: beginner, 2: amateur, 3: semi-pro, 4:pro")
-// console.log(myExperience);
-
-let myName = "Jez";
-let myExperience = 1
-
-let message;
-
-if (myExperience == 1){
-    message = "Logged in as " + myName + " (Beginner level)";
-} else if (myExperience == 2){
-    message = "Logged in as " + myName + " (Amateur level)";
-} else if (myExperience == 3){
-    message = "Logged in as " + myName + " (Semi-professional level)";
-} else if (myExperience == 4){
-    message = "Logged in as " + myName + " (Professional level)";
-} else {
-    message = "Logged in as " + myName;
-    myExperience = prompt("How experienced a player are you? (1-4) 1: beginner, 2: amateur, 3: semi-pro, 4:pro")
+// function gets and writes user name
+function getName() {
+  let myName = prompt("What is your name?");
+  document.write(myName);
 }
-document.write(message);
 
-// if (myTime <= 11){
-//     console.log(`It's ${myTime} o'clock. Good morning, ${myName}`)
-// } else if (myTime === 12){
-//     console.log(`It's midday. Enjoy your lunch, ${myName}`)
-// } else if (myTime <=18){
-//     console.log(`It's ${myTime} o'clock. Good afternoon, ${myName}`)
-// } else if (myTime <24){
-//     console.log(`It's ${myTime} o'clock. Good evening, ${myName}`)
-// } else {
-//     console.log(`Oops. Are you sure that ${myTime} is a number between 0 and 23, ${myName}? Please try again`)
-//     myTime = prompt("What hour of the day is it? 0-23")
-// }
-console.log("Hello world")
+// function asks experience as number, interprets to words and writes
+function getExperience() {
+  let experience = "";
+  while (experience == "") {
+    let myExperience = prompt(
+      "How experienced a player are you? (1-4) 1: beginner, 2: amateur, 3: semi-pro, 4:pro"
+    );
+    if (myExperience == 1) {
+      experience = " (Beginner level)";
+    } else if (myExperience == 2) {
+      experience = " (Amateur level)";
+    } else if (myExperience == 3) {
+      experience = " (Semi professional level)";
+    } else if (myExperience == 4) {
+      experience = " (Professional level)";
+    } else {
+      experience = "";
+      myExperience = prompt(
+        "How experienced a player are you? (1-4) 1: beginner, 2: amateur, 3: semi-pro, 4:pro"
+      );
+    }
+    document.write(experience);
+  }
+}
+// function sets background colour as chosen by user
+function chooseBackground() {
+  let colourCode = "#3ab1c8";
+  let myColour = prompt(
+    "Choose background colour: 1 = blue, 2 = green, 3 = red, 4 = white"
+  );
+  if (myColour == 1) {
+    colourCode = "#5497A7";
+  } else if (myColour == 2) {
+    colourCode = "#7AE582";
+  } else if (myColour == 3) {
+    colourCode = "#721817";
+  } else if (myColour == 4) {
+    colourCode = "#EEFFDB";
+  }
 
-
+  document.body.style.backgroundColor = colourCode;
+}
